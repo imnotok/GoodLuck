20bc78e6255ec63b89c7e9b91b806cf780421585
BJ.y <545350081@qq.com>
2016-05-18 15:31:13 +0800

    base project

---
+++ GoodLuck/GoodLuck/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodLuck
+//
+//  Created by BJ.y on 16/5/16.
+//  Copyright © 2016年 BJ.y. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GoodLuck/Pods/Target Support Files/Pods-GoodLuck/Pods-GoodLuck-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GoodLuckVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GoodLuckVersionString[];
+
